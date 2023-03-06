aba04d59681c359c3d4904d57b1d10f0dc4c6312
dantaswilljpg <dantaswill748@gmail.com>
2023-03-05 23:13:58 -0300

    Banco de dados-sprint1

--- BANCO DE DADOS-SPRINT 1.sql
+++ BANCO DE DADOS-SPRINT 1.sql
@@ -1,29 +1,29 @@
-create database cocoa;
-use cocoa;
+CREATE DATABASE cocoa;
+USE cocoa;
 
 
 
 -- TABELA USUARIO
 CREATE TABLE Usuario (
-    idUsuario int primary key auto_increment,
+    idUsuario INT PRIMARY KEY AUTO_INCREMENT,
     nomeUsuario VARCHAR(45),
-    email VARCHAR(45), CONSTRAINT chkEmailUsuario check (email like '%@%'),
+    email VARCHAR(45), CONSTRAINT chkEmailUsuario CHECK (email LIKE '%@%'),
     senha VARCHAR(45),
-    telcel char(11)
+    telCelular CHAR (11)
     );
 
 -- TABELA EMPRESA
 CREATE TABLE Empresa (
-    idEmpresa int primary key auto_increment,
-	CNPJ char(14),
+    idEmpresa INT PRIMARY KEY AUTO_INCREMENT,
+	CNPJ CHAR (14),
     nomeComercialEmpresa VARCHAR(50),
     responsavel VARCHAR(50),
-    telfixo char(8),
-    Cidade varchar(20),
-    Estado varchar(20),
+    telfixo CHAR (8),
+    Cidade VARCHAR (20),
+    Estado VARCHAR (20),
     CEP CHAR(9),
-    Numero int,
-    Complemento varchar(45)
+    Numero INT,
+    Complemento VARCHAR (45)
 );
 
 -- TABELA PERFIL(
@@ -59,15 +59,15 @@ CREATE TABLE Leitura(
 );
 
 
-
-insert into Usuario (nomeUsuario,email,senha,telcel) values
+-- INSERT TABELA USUARIO
+INSERT INTO Usuario (nomeUsuario,email,senha,telCelular) VALUES
 ("Will dantas","WillDantas@gmail.com","DantasWill123",11960387699), 
 ("Vitor Quintas","VitorQuintass@gmail.com","QuintasVitor253",11936895627),
 ("Thais Moitinho","Thais_Moitinho@gmail.com","MoitinhoThais050",11945474698),
 ("Kauan De Queiroz","Kauan.Queiroz@gmail.com","QueirozKaun203",11974423412);
 
 -- INSERT TABELA EMPRESA
-insert into empresa (cnpj,nomeComercialEmpresa,responsavel,telfixo,cidade,estado,cep,Numero,Complemento) VALUES
+INSERT INTO empresa (cnpj,nomeComercialEmpresa,responsavel,telfixo,cidade,estado,cep,Numero,Complemento) VALUES
 (7201756800786,'Cocoa Solution','Samuel Silveira','25548705','São Paulo','SP',084603445,'156','Fazenda'), 
 (04634178000258,'Previsons Cocoa','Marcelo Alcantara','26574123','São Paulo','SP',09654896,'157','Sitio'),
 (26390076800756, 'Box liverycocoa','Leonardo Brito','25798750','Ubatuba','SP',09765345,'158','Fazenda'),
@@ -82,21 +82,21 @@ insert into Perfil(idPerfil,cargo,nomeColaborador) VALUES
 (4,'Analista','Nelton Miguel');
 
 SELECT * FROM Autonomo;
-insert into Autonomo(idAutonomo,cpf,nome,email,senha) VALUES
+INSERT INTO Autonomo(idAutonomo,cpf,nome,email,senha) VALUES
 (1,'27238354802','Vinicius Cruz','ViniciusCruzz@gmail.com','ViniCruz123'),
 (2,'39650651810','Vitoria Bessane','VitoriaBessane@gmail.com','VihBessane123'),
 (3,'57029871871','Pedro Frias','PedroFrias@gmail.com','PedroF123'),
 (4,'57029871871','Cauan Gustavo','CauanGu@gmail.com','Cauangus123');
 
 -- INSERT TABELA SENSOR
-insert into Sensor(idSensor,statusSensor) values 
+INSERT INTO Sensor(idSensor,statusSensor) VALUES
 (1,'Ativo'),
 (2,'Ativo'),
 (3,'Manutenção'),
 (4,'Inativo');
 
 -- INSERT TABELA LEITURA
-insert into Leitura values 
+INSERT INTO  Leitura(idLeitura,temperatura,umidade,dtHora) VALUES
 (1, '21.50', '50.5', '2023-03-01 12:30:00'),
 (2, '22.15', '50.5', '2023-03-01 11:00:00'),
 (3, '24.96', '50.5', '2023-03-02 16:30:00'),
